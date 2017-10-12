b4805cedbdfa30a378cd7e5e059e8cc68e671879
Raheel Javed <ch_raheel@hotmail.com>
2017-10-12 09:49:39 +0400

    Release v3.0.17

--- dist/ng-wig.js
+++ dist/ng-wig.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * version: 3.0.16
+ * version: 3.0.17
  */
-var VERSION = '3.0.15';
+var VERSION = '3.0.16';
 angular.module('ngWig', ['ngwig-app-templates']);
 angular.ngWig = {
   version: VERSION
